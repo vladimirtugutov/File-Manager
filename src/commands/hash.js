9199716f6d7626fd34ba4c57c929c757dbdde1e4
marmot T <marmot@marmots-MacBook-Air.local>
2025-05-05 18:02:49 -0300

    feat: add hash and zip operations

--- src/commands/hash.js
+++ src/commands/hash.js
@@ -1,14 +1,23 @@
-import { createReadStream } from 'fs';
 import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
 import { handleError } from '../utils/error.js';
 
-export const hash = async (path) => {
+export const hash = async (filePath) => {
   try {
-    const stream = createReadStream(path);
+    const absPath = resolve(filePath);
+    const stream = createReadStream(absPath);
     const hash = createHash('sha256');
-    stream.on('data', chunk => hash.update(chunk));
+
+    stream.on('error', handleError);
+
+    stream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+
     stream.on('end', () => {
-      console.log(hash.digest('hex'));
+      const result = hash.digest('hex');
+      console.log(result);
     });
   } catch {
     handleError();
--- src/commands/zip.js
+++ src/commands/zip.js
@@ -1,28 +1,38 @@
 import { createReadStream, createWriteStream } from 'fs';
-import { brotliCompress, brotliDecompress } from 'zlib';
-import { pipeline } from 'stream/promises';
+import { resolve } from 'path';
+import { createBrotliCompress, createBrotliDecompress } from 'zlib';
+import { pipeline } from 'stream';
 import { handleError } from '../utils/error.js';
+import { promisify } from 'util';
 
-export const compress = async (src, dest) => {
+const pipe = promisify(pipeline);
+
+export const compress = async (source, destination) => {
   try {
-    await pipeline(
-      createReadStream(src),
-      brotliCompress(),
-      createWriteStream(dest)
-    );
+    const src = resolve(source);
+    const dest = resolve(destination);
+
+    const readStream = createReadStream(src);
+    const writeStream = createWriteStream(dest);
+    const brotli = createBrotliCompress();
+
+    await pipe(readStream, brotli, writeStream);
   } catch {
     handleError();
   }
 };
 
-export const decompress = async (src, dest) => {
-  try {
-    await pipeline(
-      createReadStream(src),
-      brotliDecompress(),
-      createWriteStream(dest)
-    );
-  } catch {
-    handleError();
-  }
+export const decompress = async (source, destination) => {
+    try {
+      const src = resolve(source);
+      const dest = resolve(destination);
+  
+      const readStream = createReadStream(src);
+      const writeStream = createWriteStream(dest);
+      const brotli = createBrotliDecompress();
+  
+      await pipe(readStream, brotli, writeStream);
+    } catch {
+      handleError();
+    }
 };
