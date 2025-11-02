9426c4f0f30d454565e0778c2ff376ad6b43f52d
Vladimir Tugutov <tugutov@gmail.com>
2025-11-02 17:02:11 -0300

    fix: promisify hash

--- src/commands/hash.js
+++ src/commands/hash.js
@@ -4,22 +4,29 @@ import { resolve } from 'path';
 import { handleError } from '../utils/error.js';
 
 export const hash = async (filePath) => {
-  try {
-    const absPath = resolve(filePath);
-    const stream = createReadStream(absPath);
-    const hash = createHash('sha256');
+  return new Promise((resolvePromise, rejectPromise) => {
+    try {
+      const absPath = resolve(filePath);
+      const stream = createReadStream(absPath);
+      const hashObj = createHash('sha256');
 
-    stream.on('error', handleError);
+      stream.on('error', () => {
+        handleError();
+        rejectPromise();
+      });
 
-    stream.on('data', (chunk) => {
-      hash.update(chunk);
-    });
+      stream.on('data', (chunk) => {
+        hashObj.update(chunk);
+      });
 
-    stream.on('end', () => {
-      const result = hash.digest('hex');
-      console.log(result);
-    });
-  } catch {
-    handleError();
-  }
+      stream.on('end', () => {
+        const result = hashObj.digest('hex');
+        console.log(result);
+        resolvePromise();
+      });
+    } catch {
+      handleError();
+      rejectPromise();
+    }
+  });
 };
